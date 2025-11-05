3a7493a34520304fd456e0d82a4e6d85196f19d8
ajhsbrir-oss <ajhsbrir@gmail.com>
2025-11-05 17:37:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚙️ ajuste para o GitHub Pages
+// Configuração para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/festivalamoda/', // <-- coloque exatamente o nome do seu repositório aqui!
+  base: '/festivalamoda/',
 })
